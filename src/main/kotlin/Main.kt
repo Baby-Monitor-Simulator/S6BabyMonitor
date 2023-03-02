4c9073f4bc05298114d3b23d441589f64296f9ff
dainank <dainank@proton.me>
2023-03-02 13:44:14 +0100

    local library

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,11 @@
+import com.mathworks.engine.MatlabEngine
+
 fun main(args: Array<String>)  {
-    javaPassArg.main();
+    val eng = MatlabEngine.startMatlab()
+    val p = doubleArrayOf(1.0, -1.0, -6.0)
+    val r: DoubleArray = eng.feval<DoubleArray>("roots", p)
+    for (e in r) {
+        println(e)
+    }
+    eng.close()
 }
