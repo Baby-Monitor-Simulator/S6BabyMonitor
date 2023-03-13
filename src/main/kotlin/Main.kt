1cec9c06294b3737e6b4c4ec2a3a9051f3953a0e
Tim de Moor <timdemoor122@gmail.com>
2023-03-13 14:59:15 +0100

    Made first connection with matlab

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,5 +7,21 @@ fun main()  {
     for (e in r) {
         println(e)
     }
+
+    val i1: Short = 3
+    val i2: Short = 5
+    val i3: Short = 10
+
+    val area: Int = eng.feval("calcArea", i1, i2)
+    val cube: Int = eng.feval("calcCube", i1, i2, i3)
+    val pow: Int = eng.feval("calcPower", i3, i1)
+
+    println(area)
+    println(cube)
+    println(pow)
+
+
+    eng.evalAsync("FMPmodel")
+
     eng.close()
 }
