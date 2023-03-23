d8b0441b69bebcda5c8b2992653e47d7b889bbaf
Tim de Moor <timdemoor122@gmail.com>
2023-03-23 10:12:45 +0100

    Small syntax optimizations and adhered more to kotlin conventions

--- src/main/kotlin/MatlabConnection.kt
+++ src/main/kotlin/MatlabConnection.kt
@@ -5,8 +5,12 @@ class MatlabConnection(
 ) {
 
     fun runModel(model: MatlabModel) = engine.eval(model.type)
+
     fun stop() = engine.close()
+
     fun getSingleVar(varName: MatlabSingleVariable) = engine.getVariable<Double>(varName.type)
+
     fun getDoubleArray(varName: MatlabDoubleArray) = engine.getVariable<DoubleArray>(varName.type)
+
     fun getMultiDimDoubleArray(varName: MatlabMultiDimDoubleArray) = engine.getVariable<Array<DoubleArray>>(varName.type)
 }
--- src/main/kotlin/MatlabModel.kt
+++ src/main/kotlin/MatlabModel.kt
@@ -1,4 +1,3 @@
-//List of names for matlab models
 enum class MatlabModel(val type: String) {
     MainModel("FMPmodel"),
     TestModel("TestModel")
